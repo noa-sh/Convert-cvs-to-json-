47c94a3b571df945628f8ad9fd710e6af6ea6bf9
noa-sh <58655284+noa-sh@users.noreply.github.com>
2019-12-08 15:00:39 +0200

    convert cvs to json
    import packages to read csv file and write a new json file

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+CSV_LOCATION = "your_csv.csv"
+FULL_JSON_LOCATION =  "location_of_your_json_file.json"
+DROP_JSON_LOCATION =  "location_of_your_json_file_droped.json"
+COLUMN_TO_DROP = "colunm name"
+
+csv_file = CSV_LOCATION
+data = pd.read_csv(csv_file)
+json_file = FULL_JSON_LOCATION
+data.to_json(json_file)
+
+new_data = data.drop(COLUMN_TO_DROP , axis=1)
+
+json_file_droped = DROP_JSON_LOCATION
+new_data.to_json(json_file_droped)
