e78097be7ac1c4aa7bf0a3aa812305de9dd83ad4
noa-sh <58655284+noa-sh@users.noreply.github.com>
2019-12-09 12:19:29 +0200

    fix json file
    fix json file

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-CSV_LOCATION = "your_csv.csv"
-FULL_JSON_LOCATION =  "location_of_your_json_file.json"
-DROP_JSON_LOCATION =  "location_of_your_json_file_droped.json"
-COLUMN_TO_DROP = "colunm name"
+CSV_LOCATION = "your_csvfile.csv"
+FULL_JSON_LOCATION =  "location_json_file.json"
+DROP_JSON_LOCATION =  "location_json_file_droped.json"
+COLUMN_TO_DROP = "colunm_name"
 
 csv_file = CSV_LOCATION
 data = pd.read_csv(csv_file)
 json_file = FULL_JSON_LOCATION
-data.to_json(json_file)
+data.to_json(json_file, orient="records")
 
 new_data = data.drop(COLUMN_TO_DROP , axis=1)
 
 json_file_droped = DROP_JSON_LOCATION
-new_data.to_json(json_file_droped)
+new_data.to_json(json_file_droped, orient="records")
